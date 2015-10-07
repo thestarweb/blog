ff986694b220b89128524d9a1bf79c7db371f07e
thestarweb <597577939@qq.com>
2015-10-07 17:01:48 +0800

    分类信息浏览
    注意请使用新版system类，否则将会缺少函数而无法运行！！

--- admin_file/2.php
+++ admin_file/2.php
@@ -1,4 +1,20 @@
 <?php
 $type=new type_server($system);
-$list=$type->get_list_p(20);
-include $system->get_view('admin/type');
+switch (isset($_GET['doing'])?$_GET['doing']:'') {
+	case 'add':
+		# code...
+		break;
+	case 'select':
+		$info=(isset($_GET['tid'])&&$_GET['tid']!=0)?$type->get_info_by_id($_GET['tid']):array('name' =>'root','info'=>'这是根分类，便于管理使用','id'=>0);;
+		include $system->get_view('admin/type_info');
+		break;
+	case 'delete':
+		# code...
+		break;
+	default:
+		$list=$type->get_list_p(20);
+		$surl=$system->url_addget('doing','select');
+		include $system->get_view('admin/type_list');
+		//echo  $system->url_addget('doing','select');
+		break;
+}
