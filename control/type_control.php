20e19d9f4933a35ef3807693a066c1770f7f98dd
thestarweb <597577939@qq.com>
2015-12-05 19:43:15 +0800

    优化

--- control/type_control.php
+++ control/type_control.php
@@ -1,16 +1,5 @@
 <?php
 class type_control{
-	public function __call($id,$c){
-		/*$system=$c[0];
-		$type=new type_server($system);
-		$type_info=$type->get_info_by_id($id);
-		$ctypes=$type->get_clist($id);
-		$list=$type->get_essay($id);
-		//$title_name='xx分类下的文章';
-		$system->show_head('分类 '.$type_info['name'].' 的文章');
-		require $system->get_view('type');
-		$system->show_foot();*/
-	}
 	public function id_page($system,$c){
 		$cs=explode('/',$c);
 		$id=$cs[0]+0;
@@ -26,13 +15,13 @@ class type_control{
 					$system->show_head('分类 '.$type_info['name'].' 的文章——星星站点博客');
 					include $system->get_view('type');
 					$system->show_foot();
+					exit;
 				}
-				exit;
 			}
 		}else{
 			header("HTTP/1.1 301 Moved Permanently"); 
-		header('location: '.URLROOT.'type/id/'.$id.'/1');
-		exit;
+			header('location: '.URLROOT.'type/id/'.$id.'/1');
+			exit;
 		}
 		$error_info='<h1>:) 真的什么都没发现啊</h1><br/>请确认您输入的地址是否有误（分类下没有文章也会出现此错误）';
 		include $system->get_view('error');
@@ -53,13 +42,13 @@ class type_control{
 					$system->show_head('分类 '.$type_info['name'].' 的文章——星星站点博客');
 					include $system->get_view('type');
 					$system->show_foot();
+					exit;
 				}
-				exit;
 			}
 		}else{
 			header("HTTP/1.1 301 Moved Permanently"); 
-		header('location: '.URLROOT.'type/name/'.$name.'/1');
-		exit;
+			header('location: '.URLROOT.'type/name/'.$name.'/1');
+			exit;
 		}
 		$error_info='<h1>:) 真的什么都没发现啊</h1><br/>请确认您输入的地址是否有误（分类下没有文章也会出现此错误）';
 		include $system->get_view('error');
