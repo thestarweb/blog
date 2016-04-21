4ded81b6db4f3becf4f45e9651ee52c6c8b5a646
thestarweb <597577939@qq.com>
2016-04-21 11:46:50 +0800

    推送文章展示api

--- control/api_control.php
+++ control/api_control.php
@@ -18,4 +18,16 @@ class api_control{
 			$cache->end_page_cache();
 		}
 	}
+	public function get_pushs_page($system,$c){
+		//	var_dump($_SERVER);exit;
+		header('Access-Control-Allow-Origin: *');
+		$uroot=($_SERVER['SERVER_PROTOCOL']=='HTTP/1.1'?'http://':'https://').$_SERVER['SERVER_NAME'].URLROOT.'essay/id';
+		$e=new essay_server($system);
+		$list=$e->get_push($c);
+		foreach ($list as &$v) {
+			$v['url']=$uroot.$v['id'];
+			unset($v['content']);
+		}
+		$system->show_json($list);
+	}
 }
