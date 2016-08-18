e5ce8597ba5c2bfc3dcd208884c08a91eac32a43
thestarweb <597577939@qq.com>
2016-08-18 17:17:05 +0800

    终于有关于页面了

--- control/index_control.php
+++ control/index_control.php
@@ -11,8 +11,8 @@ class index_control{
 		$system->show_foot();
 	}
 	public function about_page($system,$c){
-		echo '这是关于界面';
-		echo isset($_SERVER['HTTP_X_REQUESTED_WITH'])?'手机访问'.$_SERVER['HTTP_X_REQUESTED_WITH']:'电脑访问';
-		echo $c;
+		$system->show_head('关于——星星站点博客');
+		include $system->get_view('about');
+		$system->show_foot();
 	}
 }
