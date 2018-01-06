5cc3404080073b4278d89158238c49954fececd5
thestarweb <597577939@qq.com>
2018-01-06 23:01:10 +0800

    更新程序修改

--- control/install_control.php
+++ control/install_control.php
@@ -4,15 +4,15 @@ class install_control{
 		echo '自动导入数据库等功能暂时还无法正常使用（过一段时间有空也会努力开发此功能），请第一次安装请先手动导入SQL语句后执行<a href="'.URLROOT.'install/update">更新数据库</a>，以后数据库更新都会更新XML文件，通过运行该页面可以完成自动更新数据库的功能';
 	}
 	public function update_page($system){
-		if(file_exists(__DIR__.'/update.lock')){
+		if(file_exists(__DIR__.'/../update.lock')){
 			echo '更新功能已被锁定，如需更新数据库，请删除update.lock';
 			exit;
 		}
 		if(isset($_GET['yes'])){
 			set_time_limit(0);
-			dbhelper_tool::update($system->db(),'./database.xml');
+			dbhelper_tool::update($system->db(),__DIR__'/../database.xml');
 			echo '数据库更新已完成';
-			fclose(fopen(__DIR__.'/update.lock','w'));
+			fclose(fopen(__DIR__.'/../update.lock','w'));
 			exit;
 		}
 		echo '这里是自动更新数据库的地方，如果需要更新请<a href="?yes=1">点我</a><br/>';
